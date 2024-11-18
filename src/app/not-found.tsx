ea813adb3064e328db80f5a27d0593a36a407344
jeongho3786 <jeongho3786@gmail.com>
2024-11-18 20:29:24 +0900

    feat: 로그인, 404 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function Error404() {
+  return <div>나야 404</div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {}, []);
+
   return <div>안녕 세상아</div>;
 }
