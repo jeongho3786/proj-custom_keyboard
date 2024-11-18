7118ab73e1aade3378d36fa1dc92c325dfebabf8
jeongho3786 <jeongho3786@gmail.com>
2024-11-18 17:36:34 +0900

    init repo

---
+++ public/fonts/index.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+  src: "./PretendardVariable.woff2",
+  fallback: ["Arial", "system-ui", "sans-serif"],
+  display: "swap",
+});
